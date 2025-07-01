51cb6b2cf3c2b9ee8be1146f722196ae0e67d4ed
Gopikasri03 <146168850+Gopikasri03@users.noreply.github.com>
2025-06-30 21:53:05 -1200

    Add files via upload

---
+++ artask.1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+int i,n;
+int arr1[5],arr2[5];
+
+{
+for (i=0;i<n;i++)
+{
+printf("elements of the array:\n");
+scanf("%d",&arr1[i]);
+}
+for(i=0;i<n;i++)
+{
+arr2[i] = arr1[i];
+}
+printf("enter the elements stored");
+}
+{
+for(i=0;i<n;i++)
+{
+ printf("%d",arr1[i]) ;
+}
+for(i=0;i<n;i++)
+{
+printf("%d",arr2[i]);
+}
+}}
